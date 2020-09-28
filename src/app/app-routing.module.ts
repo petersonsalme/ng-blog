dc71dcae657ab3dcf9fc95b31b17bce0cb905120
Peterson Salme <petersonsalme@gmail.com>
2020-09-28 16:14:47 -0300

    implementing all blog components using meta tag and open graphs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: ArticleListComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: ':key', component: ArticleComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/mock-articles.ts
@@ -0,0 +1,22 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: 'My First Article',
+        content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lectus eu quam semper hendrerit. Nam sed massa in nunc pellentesque vestibulum. Donec justo ipsum, dictum a sapien ac, vestibulum ultricies urna. Nam consectetur libero eu ullamcorper vestibulum. Mauris vulputate tortor ac elit porta, vel scelerisque ante aliquet. Suspendisse ac leo dolor. In pellentesque augue in tortor congue iaculis. Vestibulum in porta nunc. Pellentesque fringilla mi sit amet ligula iaculis varius.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lectus eu quam semper hendrerit. Nam sed massa in nunc pellentesque vestibulum. Donec justo ipsum, dictum a sapien ac, vestibulum ultricies urna. Nam consectetur libero eu ullamcorper vestibulum. Mauris vulputate tortor ac elit porta, vel scelerisque ante aliquet. Suspendisse ac leo dolor. In pellentesque augue in tortor congue iaculis. Vestibulum in porta nunc. Pellentesque fringilla mi sit amet ligula iaculis varius.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lectus eu quam semper hendrerit. Nam sed massa in nunc pellentesque vestibulum. Donec justo ipsum, dictum a sapien ac, vestibulum ultricies urna. Nam consectetur libero eu ullamcorper vestibulum. Mauris vulputate tortor ac elit porta, vel scelerisque ante aliquet. Suspendisse ac leo dolor. In pellentesque augue in tortor congue iaculis. Vestibulum in porta nunc. Pellentesque fringilla mi sit amet ligula iaculis varius.</p>',
+        description: 'This is my first article! It\'s great. Please read it.',
+        key: 'my-first-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+        id: 1,
+        title: 'My Second Article',
+        content: '',
+        description: 'Also a great article.',
+        key: 'my-second-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular_solidBlack.png'
+    }
+];
