b04faaf55f3daa2de8378eaf25c02ee9b36c6059
Peterson Salme <petersonsalme@gmail.com>
2020-09-28 19:01:28 -0300

    implementing cors

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+
+var corsOptions = {
+    origin: ['http://localhost:4000', 'http://localhost:4200']
+};
 
 const app = express();
+app.use( cors(corsOptions) );
 
 app.listen(8000, () => {
     console.log('Server started');
 });
+
+require('./articles.js')(app);
