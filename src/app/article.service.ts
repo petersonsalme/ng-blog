ae0eea3dafbd4a4cd7a7e7c9d8dd7d2c66a12441
Peterson Salme <petersonsalme@gmail.com>
2020-09-29 11:23:32 -0300

    using environment to store api url

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Article } from './article';
 
 @Injectable({
@@ -10,11 +11,11 @@ export class ArticleService {
   constructor(private http: HttpClient) {}
 
   getArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>('http://localhost:8000/articles');
+    return this.http.get<Article[]>(environment.apiUrl + '/articles');
   }
 
   getArticle(key: string): Observable<Article> {
-    return this.http.get<Article>('http://localhost:8000/articles/' + key);
+    return this.http.get<Article>(environment.apiUrl + '/articles/' + key);
   }
 
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Article } from '../article';
 
 @Injectable({
@@ -11,7 +12,7 @@ export class DashboardService {
   constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>('http://localhost:8000/dashboard/overview');
+    return this.http.get<Article[]>(environment.apiUrl + '/dashboard/overview');
   }
 
 }
